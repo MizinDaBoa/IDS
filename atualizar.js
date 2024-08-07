10dc396877a5bcf43364e84433d5510d0c0d82f2
MiguelDaBoa <braidinho12@gmail.com>
2024-08-07 17:01:10 -0300

    Update atualizar.js

--- atualizar.js
+++ atualizar.js
@@ -2,7 +2,13 @@ let contatos = require('./contatos')
 
 function atualizar(id,contato){
     contato.id = contatos.length 
-    contatos[id] = contato
+    let jaExisteAtu = contatos.find(cont => cont.email === contato.email)
+    if(jaExisteAtu){
+        console.log('ja existe um usario com esse email.')
+    }else{
+        contatos[id] = contato
+    }
+    
 }
 
 module.exports = atualizar
